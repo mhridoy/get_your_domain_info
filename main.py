0847979a05f929df74dc68876b211b7988cb554e
Md.Moshiur Rahman <35825505+mhridoy@users.noreply.github.com>
2024-09-15 17:52:37 +0600

    Update main.py

--- main.py
+++ main.py
@@ -59,6 +59,9 @@ else:
 
 # Vercel serverless function handler
 from flask import Flask
+from werkzeug.middleware.proxy_fix import ProxyFix
+
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 
 def handler(event, context):
     return app(event, context)
